9ac3ff646aa364c73b166500ecae8345518c61ec
Andrei Jaume <andrei.jaumewillenska@utexas.edu>
2024-10-27 15:59:31 -0500

    Overhaul main logic

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ Entry point to the Flies and AI application
 This is the main runner with argument options. It will allow us to run the different comamnds
 """
 
-import os
 import logging
 import argparse
+
+from common.command import Command, validate_command
+from common.config import ConfigHelper
+from library.base_io import BaseIO
 
 
 def run_application(args: str) -> None:
@@ -13,15 +16,39 @@ def run_application(args: str) -> None:
     Runs the entire application based on the input arguments
 
     """
-    if args.verbose:
-        logging.basicConfig(encoding="utf-8", level=logging.DEBUG)
-    else:
-        logging.basicConfig(encoding="utf-8", level=logging.ERROR)
+    command = Command(args.command)
+    config = ConfigHelper(args.config_path)
+
+    match (command):
+        case Command.DOWNLOAD:
+            logging.info(f"Downloading dataset: {args.dataset_path}")
+            # Download the dataset
+        case Command.DOWNLOAD:
+            logging.info(f"Predicting dataset: {args.predict_path}")
+            # Predict the dataset
+        case _:
+            logging.error(f"Command not found: {args.command}")
 
 
-def validate_args(args: str) -> bool:
+def validate_args(args: argparse.Namespace) -> bool:
     """
     Validate that the required arguemnts are passed in"""
+    if not args.command or not validate_command(args.command):
+        logging.error("Please provide a valid command")
+        return False
+
+    baseIO = BaseIO()
+
+    if not baseIO.is_path_valid(args.config_path):
+        logging.error(f"Config path {args.config_path} is not valid")
+        return False
+
+    operating_path = (
+        args.dataset_path if args.command == Command.DOWNLOAD else args.predict_path
+    )
+    if not baseIO.is_path_valid(operating_path):
+        logging.DEBUG(f"Input path {operating_path} is not valid. Creating it...")
+        baseIO.create_directory(operating_path)
 
     return True
 
@@ -45,7 +72,7 @@ if __name__ == "__main__":
 
     # Subparser for the download command
     download_parser = subparsers.add_parser(
-        "download", help="Download and create a dataset"
+        str(Command.DOWNLOAD), help="Download and create a dataset"
     )
     download_parser.add_argument(
         "-d",
@@ -55,12 +82,18 @@ if __name__ == "__main__":
     )
 
     # Subparser for the classify command
-    classify_parser = subparsers.add_parser("classify", help="Classify a dataset")
+    classify_parser = subparsers.add_parser(
+        str(Command.PREDICT), help="Predict a dataset"
+    )
     classify_parser.add_argument(
-        "-p", "--classify_path", help="Path to the dataset to classify", required=True
+        "-p", "--predict_path", help="Path to the dataset to predict", required=True
     )
 
     args = parser.parse_args()
+    if args.verbose:
+        logging.basicConfig(encoding="utf-8", level=logging.DEBUG)
+    else:
+        logging.basicConfig(encoding="utf-8", level=logging.ERROR)
 
     if validate_args(args):
         run_application(args)
