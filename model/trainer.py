eae5dc1ab50e3ccd1471523758aa56d4fa3cb426
Andrei Jaume <andrei.jaumewillenska@utexas.edu>
2024-11-29 17:28:39 -0600

    feat(validation): add validation code for the model

--- model/trainer.py
+++ model/trainer.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, random_split
 from torchvision import transforms
+from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 
@@ -37,7 +38,7 @@ class ModelTrainer:
         dataset = SpeciesDataset(self.dataset_dir, transform=self.transform)
 
         # Define the split sizes
-        train_size = int(0.8 * len(dataset))
+        train_size = int(0.75 * len(dataset))
         val_size = len(dataset) - train_size
         batch_size = 256
         train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
@@ -60,18 +61,20 @@ class ModelTrainer:
             self.criterion,
             self.optimizer,
             self.model_path,
-            self.output_path,
             self.num_epochs,
+        )
+
+        avg_loss, accuracy = self.evaluate_model(
+            self.model, val_loader, self.criterion, self.output_path
         )
 
     def train_model(
         self,
-        model,
-        dataloader,
-        criterion,
-        optimizer,
+        model: CNN,
+        dataloader: DataLoader,
+        criterion: nn.Module,
+        optimizer: optim.Optimizer,
         model_path: str,
-        output_path: str,
         num_epochs: int,
     ):
         model.train()
@@ -80,7 +83,7 @@ class ModelTrainer:
         )
         for epoch in range(num_epochs):
             running_loss = 0.0
-            for inputs, labels in dataloader:
+            for inputs, labels in tqdm(dataloader):
                 inputs, labels = inputs.to(self.device), labels.to(self.device)
 
                 optimizer.zero_grad()
@@ -98,11 +101,36 @@ class ModelTrainer:
         torch.save(model.state_dict(), model_path)
         logger.info(f"Model saved to: {model_path}")
 
+    def evaluate_model(
+        self, model, dataloader: DataLoader, criterion: nn.Module, output_path: str
+    ):
+        """Evaluate the input model with the input data"""
+        model.eval()
+        running_loss = 0.0
+        correct_predictions = 0
+        total_predictions = 0
+
+        with torch.no_grad():
+            for inputs, labels in tqdm(dataloader):
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                running_loss += loss.item() * inputs.size(0)
+
+                _, predicted = torch.max(outputs, 1)
+                _, labels_max = torch.max(labels, 1)
+                correct_predictions += (predicted == labels_max).sum().item()
+                total_predictions += labels.size(0)
+
+        avg_loss = running_loss / len(dataloader.dataset)
+        accuracy = correct_predictions / total_predictions
+
+        logger.info(f"Evaluation Loss: {avg_loss:.4f}, Accuracy: {accuracy:.4f}")
+
         with open(output_path, "w") as f:
-            f.write(f"Model Loss: {epoch_loss:.4f}\n")
-            f.write(f"Model Path: {model_path}")
+            f.write(f"Evaluation Loss: {avg_loss:.4f}, Accuracy: {accuracy:.4f}")
+
         logger.debug(f"Output saved to: {output_path}")
 
-    def evaluate_model(self, model, dataloader, criterion):
-        """Evaluate the input model with the input data"""
-        pass
+        return avg_loss, accuracy
